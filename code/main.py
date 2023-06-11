b4fcb478f9a13ec470032e7defc06fc26e3f7af7
murilopbs <62524293+murilopbs@users.noreply.github.com>
2023-06-11 16:05:08 -0300

    Adicionando lógica na classe Troca_Loja_Fisica

--- code/main.py
+++ code/main.py
@@ -40,13 +40,33 @@ class Fluxo_de_Troca(ABC):
     
 class Troca_Loja_Fisica(Fluxo_de_Troca):
     def realizar_troca(self):
-        print("Realizando troca em loja física para o produto:", self._produto.nome)
+        printaCondicoes()
+        decisao = input("Digite '1' se seu produto atende a todas as condições e você deseja a troca pelo mesmo produto.\nDigite '2' se seu produto atende a todas as condições e você deseja a troca por outro produto.\nDigite '3' se ele não atende a todas as condições.")
+        if decisao == '1':
+            print("Realizando troca em loja física para o produto '{}'" .format(self._produto.nome))
+        if decisao == '2':
+            novoProduto = input("Digite o nome do produto desejado: ")
+            print("Realizando troca em loja física do produto '{}' para o produto '{}'".format(self._produto.nome, novoProduto))
+        if decisao == '3':
+            print("Infelizmente a troca em loja física para o produto '{}' não é possível de ser realizada" .format(self._produto.nome))
         pass
     
 class Troca_Marketplace(Fluxo_de_Troca):
     def realizar_troca(self):
         print("Realizando troca em marketplace para o produto:", self._produto.nome)
         pass
+
+def printaCondicoes():
+    condicoes = []
+    condicoes.append("90 dias corridos já se passaram a partir da compra?")
+    condicoes.append("A embalagem é a original, sem uso e com a etiqueta de identificação fixada?")
+    condicoes.append("Possui a nota fiscal do produto?")
+    condicoes.append("Seu produto é de valor igual ou maior do que o desejado?")
+    condicoes.append("Se o produto desejado for de valor maior, você dejeja completar o valor faltante?")
+    condicoes.append("Seu produto foi comprado em uma loja física?")
+    condicoes.append("O produto desejado faz parte dessa loja?")
+    for i in condicoes:
+        print(i)
 
 
 produto = Produto('banana', 'fulano', '11/06/2023', True, True)
